ebd6eafc4a9ca7cfbdb0da8380818d99ecd313d5
Nico Dann <nicodann@gmail.com>
2021-11-15 15:31:32 +0000

    made survey app

--- survey.js
+++ survey.js
@@ -5,8 +5,69 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const answerList = {};
 
-  rl.close();
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (answers) => {
+  answerList.name = answers;
+
+  rl.question('What\'s an activity you like doing? ', (answers) => {
+    answerList.activity = answers;
+
+    rl.question('What do you listen to while doing that?', (answers) => {
+      answerList.listen = answers;
+
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answers) => {
+        answerList.meal = answers;
+        
+        rl.question('What\'s your favourite thing to eat for that meal?', (answers) => {
+          answerList.food = answers;
+
+          rl.question('Which sport is your absolute favourite?', (answers) => {
+            answerList.sport = answers;
+
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answers) => {
+              answerList.super = answers;
+              rl.close();
+              console.log(`\n${answerList.name} loves listening to ${answerList.listen} while ${answerList.activity}, devouring ${answerList.food} for ${answerList.meal}, prefers ${answerList.sport} over any other sport, and is amazing at ${answerList.super}.`);
+
+              
+            });
+            
+  
+          });
+          
+  
+        });
+        
+  
+      });
+      
+  
+    });
+    
+
+  });
+  
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
